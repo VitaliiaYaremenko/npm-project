b6cd3e6370d687a638ab49a7a18ab65ca544ffb4
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-01-19 21:55:22 +0100

    created new project

---
+++ index.js
@@ -0,0 +1,11 @@
+const addition = function (a, b) {
+  return a + b;
+};
+
+const subtraction = function (c, d) {
+  return c - d;
+};
+
+const multiplication = function (j, k) {
+  return j * k;
+};
